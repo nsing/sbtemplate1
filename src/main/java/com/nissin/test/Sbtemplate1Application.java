54c591dd0eb3a2e95a31e6d9c51bffc8e1d84833
Nishant Singh <nising74@gmail.com>
2018-03-05 14:03:23 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/nissin/test/Sbtemplate1Application.java
@@ -0,0 +1,12 @@
+package com.nissin.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbtemplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbtemplate1Application.class, args);
+	}
+}
